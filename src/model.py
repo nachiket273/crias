79f9d29eafdbd5cc6e497cfba59b6545ee017afe
nachiket273 <nachiket.tanksale@gmail.com>
2024-06-18 01:06:56 +0530

    MLP Layer for LLM

--- src/model.py
+++ src/model.py
@@ -1,9 +1,9 @@
 from torch import ones, rsqrt
 from torch import Tensor, randn
-from torch.nn import Module, Parameter
+from torch.nn import Linear, Module, Parameter
 from torch.nn.functional import gelu, sigmoid
 
-__all__ = ['activation', 'RMSNorm']
+__all__ = ['activation', 'RMSNorm', 'MLP']
 
 
 def activation(x: Tensor,
@@ -60,19 +60,24 @@ class RMSNorm(Module):
         dim (int) : Embedding dimension
         eps (float) : Epsilon avoid divide by zero
                       Default: 1e-6
+        add_unit_offset (bool): Add unit offset to norm
+                                This is from google gemma paper
+                                (#https://github.com/google/gemma_pytorch)
 
     Example:
         >>> x = torch.randn((5, 2))
         >>> rmsnorm = RMSNorm(dim=2)
         >>> norm = rmsnorm(x)
     """
-    def __init__(self, dim: int, eps: float = 1e-6) -> None:
+    def __init__(self, dim: int, eps: float = 1e-6,
+                 add_unit_offset: bool = True) -> None:
         r"""
         Initialize RMSNorm layer
         """
         super().__init__()
         self.eps = eps
         self.weight = Parameter(ones(dim))
+        self.add_unit_offset = add_unit_offset
 
     def _norm(self, x: Tensor) -> Tensor:
         r""""
@@ -85,4 +90,48 @@ class RMSNorm(Module):
         Forward pass through RMSNorm.
         """
         x_fp32 = x.float()
-        return self.weight * self._norm(x_fp32).type_as(x)
+        norm = self._norm(x_fp32).type_as(x)
+        if self.add_unit_offset:
+            return (1 + self.weight) * norm
+        return self.weight * norm
+
+
+class MLP(Module):
+    r"""
+    MLP Layer for transformer.
+    TBD - use fuse linear layer as used in Gemma(https://github.com/google/gemma_pytorch)
+
+    Args:
+        hid_dim (int) : Hidden dimension size
+        use_geglu (bool) : Use geglu activation.
+                           Default: False
+                           We use Silu by default.
+        use_tanh (bool) : The tanh approximation to be used
+                          Dafault: True
+                          Note:- it's ignored for Silu.
+        mult (int) : Multiplier for calculating intermediate dim
+                     Default: 4
+
+    Example:
+        >>> x = torch.randn((5,2))
+        >>> mlp = MLP(2)
+        >>> out = mlp(x) 
+    """
+    def __init__(self, hid_dim: int, use_geglu: bool = False,
+                 use_tanh: bool = True, mult: int = 4) -> None:
+        r""""
+        Initilize MLP Layer
+        """
+        super().__init__()
+        self.c_fc = Linear(hid_dim, hid_dim * mult)
+        self.c_proj = Linear(mult * hid_dim, hid_dim)
+        self.use_geglu = use_geglu
+        self.use_tanh = use_tanh
+
+    def forward(self, x):
+        r"""
+        Forward pass for MLP layer
+        """
+        x = self.c_fc(x)
+        x = activation(x, self.use_geglu, self.use_tanh)
+        return self.c_proj(x)
